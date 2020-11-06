215a69d6de7d6ba13b5d660c83f7c863f9e4109f
Gustav Olsson <70860287+Justvejp@users.noreply.github.com>
2020-11-06 10:38:07 +0100

    Update Main.java

--- TestingAlgorithms/src/com/company/Main.java
+++ TestingAlgorithms/src/com/company/Main.java
@@ -2,14 +2,6 @@ package com.company;
 
 public class Main {
 
-    /**
-     * Nu har vi skapat en stack, som har linkedList funktionerna, du kan skapa en que, eller en stack
-     * eller bara en linkedList om du vill testa allt.
-     * Använde "My" för att inte intellij ska misstolka namnen mot sina "egna".
-     * Kunde extendat åt andra hållet om du ville vi skulle skapa en linkedList som har funktionerna av en stack eller kö
-     * men gjorde såhär nu för att kunna testa med båda två.
-     */
-
     public static void main(String[] args) {
 
         MyStack list = new MyStack();
@@ -32,6 +24,5 @@ public class Main {
         System.out.println(list.size());
         System.out.println(list.isEmpty());
 
-        //ta bort printList metoden i MyLinkedList
     }
 }
